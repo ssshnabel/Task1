9e4af70a1d091ecf3464b0e062aa2b8c5b51ddb3
MarinaS <m_shnabel@mail.ru>
2022-02-10 19:52:43 +0300

    Task is finished.

--- Hello.php
+++ Hello.php
@@ -1,42 +1,24 @@
 <?php
-    //$filenames = '/file_([0-9]|[a-z])([0-9]|[a-z])\.data/'; //регулярку не может сравнить
+    $dir = 'files';
+    $entries = array_diff(scandir($dir, 1), array('..', '.'));
 
-    //$files = '/files';
-    //$path = set_include_path(get_include_path() . PATH_SEPARATOR . $files); //не видит путь
-    //$context = stream_context_create($path);
-//    $lines = array();
-    $hashedLines = array();
     $files = array();
-    $dir = opendir('.');
-
-    while(($currentFile = readdir($dir)) !== false){
-    if ( $currentFile == '.' or $currentFile == '..' ){
-        continue;
-    }
-    $files[] = $currentFile;
-    }
-    closedir($dir);
-
-    //foreach (glob("*.file") as $filename) {
-
-        //if (preg_match($filenames, $path)){
-//            $open = fopen($filenames, "rb");
-//            $size = filesize($filenames);
-//            $binary = fread($open, $size);
-            //array_push($lines, $filename);
-//            fclose($open);
-        //}
-    //}
-
-    foreach ($files as $name){
-        //$fileContent = file_get_contents($name, false, $context);
-        $fileContent = file_get_contents($name, true);
-        $data = hash('sha3-256', $fileContent );
-        array_push($hashedLines, $data);
+    foreach ($entries as $currentFile){
+        $files[] = $currentFile;
     }
 
+    $hashedLines = array();
+    foreach ($files as $file){
+        $path = $dir."/".$file;
+        $hashedData = hash('sha3-256', file_get_contents($path));
+        $hashedLines[] = $hashedData;
+    }
 
-//$data = hash('sha3-256', file_get_contents($filenames, false, $context));
-//    $data =
-    echo var_dump($hashedLines);
+    asort($hashedLines);
+
+    $string = implode ($hashedLines);
+    $email = 'm.shnabel@yandex.ru';
+    $string = hash('sha3-256', $string.$email);
+
+    echo var_dump($string);
 ?>
